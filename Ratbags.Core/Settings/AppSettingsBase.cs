04a82b5931653d1d747bb684b5fd11512bc83665
Adrian Emmott <adrian.emmott@gmail.com>
2024-10-07 23:54:32 +0100

    generic repo

--- Ratbags.Core/Settings/AppSettingsBase.cs
+++ Ratbags.Core/Settings/AppSettingsBase.cs
@@ -2,17 +2,29 @@
 
 public class AppSettingsBase
 {
+    public ConnectionStrings ConnectionStrings { get; set; }
     public Certificate Certificate { get; set; } = new Certificate();
-
+    public JWT JWT { get; set; }
     public Messaging Messaging { get; set; } = new Messaging();
-
     public string AllowedHosts { get; set; }
+}
+
+public class ConnectionStrings
+{
+    public string DefaultConnection { get; set; }
 }
 
 public class Certificate
 {
     public string Name { get; set; }
     public string Password { get; set; }
+}
+
+public class JWT
+{
+    public string Secret { get; set; }
+    public string Issuer { get; set; }
+    public string Audience { get; set; }
 }
 
 public class Messaging
