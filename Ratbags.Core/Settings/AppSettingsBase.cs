70d9c400213ac03f718618ff6743700d29196db5
Adrian Emmott <adrian.emmott@gmail.com>
2024-10-10 22:59:36 +0100

    Added external signin to appsettingsbase

--- Ratbags.Core/Settings/AppSettingsBase.cs
+++ Ratbags.Core/Settings/AppSettingsBase.cs
@@ -3,8 +3,10 @@
 public class AppSettingsBase
 {
     public ConnectionStrings ConnectionStrings { get; set; }
+    public Ports Ports { get; set; }
     public Certificate Certificate { get; set; } = new Certificate();
     public JWT JWT { get; set; }
+    public ExternalAuthentication ExternalAuthentication { get; set; }
     public Messaging Messaging { get; set; } = new Messaging();
     public string AllowedHosts { get; set; }
 }
@@ -14,9 +16,23 @@ public class ConnectionStrings
     public string DefaultConnection { get; set; }
 }
 
+public class Ports
+{
+    public string Http { get; set; }
+    public string Https { get; set; }
+}
+
 public class Certificate
 {
     public string Name { get; set; }
+    /// <summary>
+    /// use if PEM/Key
+    /// </summary>
+    public string Key { get; set; }
+    public string Path { get; set; }
+    /// <summary>
+    /// use if PFX
+    /// </summary>
     public string Password { get; set; }
 }
 
@@ -27,10 +43,22 @@ public class JWT
     public string Audience { get; set; }
 }
 
+public class ExternalAuthentication
+{
+    public ExternalSigninProvider Google { get; set; }
+    public ExternalSigninProvider Facebook { get; set; }
+}
+
 public class Messaging
 {
     public string Hostname { get; set; }
     public string VirtualHost { get; set; }
     public string Username { get; set; }
     public string Password { get; set; }
+}
+
+public class ExternalSigninProvider
+{
+    public string ClientId { get; set; }
+    public string ClientSecret { get; set; }
 }
