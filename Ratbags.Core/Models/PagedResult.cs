a8e9d1e3bebeb44d40e12aa8e429ae70e6fb346f
Adrian Emmott <adrian.emmott@gmail.com>
2024-10-16 13:13:34 +0100

    Paged result for paginated lists

---
+++ Ratbags.Core/Models/PagedResult.cs
@@ -0,0 +1,11 @@
+﻿namespace Ratbags.Core.Models;
+
+// paginating lists of data - e.g. articles
+public class PagedResult<T>
+{
+    public int TotalCount { get; set; }
+    public List<T> Items { get; set; } = new List<T>();
+    public int PageSize { get; set; } // take
+    public int CurrentPage { get; set; } // skip
+    public int TotalPages => (int)Math.Ceiling((double)TotalCount / PageSize);
+}
