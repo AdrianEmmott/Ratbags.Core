b73ca9e7a61b0e18b96c622fa4c566f36ba105fd
Adrian Emmott <adrian.emmott@gmail.com>
2024-10-30 23:47:09 +0000

    stripping out models

--- Ratbags.Core/Models/Accounts/PasswordResetRequestModel.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.ComponentModel.DataAnnotations;
-
-namespace Ratbags.Core.Models.Accounts;
-
-public class PasswordResetRequestModel
-{
-    [Required(ErrorMessage = "Email is required")]
-    public string Email { get; set; } = null!;
-}
